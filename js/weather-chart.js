3ad296caa1b7a8938b5353abbc601cd4ea67087a
Katie Hoffman <ktahoffman@gmail.com>
2014-07-23 12:11:22 -0400

    correctly name var

--- js/weather-chart.js
+++ js/weather-chart.js
@@ -1,2 +1,2 @@
 var API_KEY = "7c209c7348c6601f";
-var URL = "http://api.wunderground.com/api/" + key + "/hourly/q/NY/New_York.json";
+var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
