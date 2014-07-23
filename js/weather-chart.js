0e5b145b6aec6f6655290723cbae56a4fa247488
Katie Hoffman <ktahoffman@gmail.com>
2014-07-23 14:47:59 -0400

    remove code that displays chart

--- js/weather-chart.js
+++ js/weather-chart.js
@@ -1,47 +1,6 @@
 jQuery(document).ready(function($) {
 
-  var API_KEY = "7c209c7348c6601f";
-  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
-  var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
-
-  var hours = [];
-  var fahrenheits = [];
-
-  $.ajax({
-    url : URL,
-    dataType : "jsonp",
-    success : function(parsed_json) {
-      var timeData = parsed_json.hourly_forecast;
-      var max = timeData.length;
-      
-      for (i = 0; i < max; i++) {
-        var fahrenheit = timeData[i].temp.english;
-        fahrenheits.push(fahrenheit);
-        var hour = timeData[i].FCTTIME.hour;
-        hours.push(hour);
-      }
-
-      var data = {
-        labels: hours,
-        datasets: [
-          {
-              label: "Hourly Weather for New York",
-              fillColor: "rgba(220,220,220,0.2)",
-              strokeColor: "rgba(220,220,220,1)",
-              pointColor: "rgba(220,220,220,1)",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "rgba(220,220,220,1)",
-              data: fahrenheits
-          }
-        ] 
-      };
-
-      var tempChart = new Chart(ctx).Line(data, {
-        bezierCurve: true
-      });    
-
-    }
-  });
+// code goes here
+ 
 });
 
