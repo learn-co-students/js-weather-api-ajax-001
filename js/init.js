0a6baadbd1a9ec7d85074176a184f62f76a68885
Steven Nunez <steven.nunez@gmail.com>
2014-11-06 12:38:23 -0500

    Fix

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "7c209c7348c6601f";
+  var API_KEY = "GO GET AN API KEY";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
