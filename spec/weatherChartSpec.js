c57d3e52bd492678a9cb66f729ea868a301aa9b7
Steven Nunez <steven.nunez@gmail.com>
2014-11-06 12:32:57 -0500

    Add tests

---
+++ spec/weatherChartSpec.js
@@ -0,0 +1,49 @@
+var dummyData = {"response":{"version":"0.1","termsofService":"http://www.wunderground.com/weather/api/d/terms.html","features":{"hourly":1}},"hourly_forecast":[{"FCTTIME":{"hour":"18","hour_padded":"18","min":"00","min_unpadded":"0","sec":"0","year":"2014","mon":"11","mon_padded":"11","mon_abbrev":"Nov","mday":"5","mday_padded":"05","yday":"308","isdst":"0","epoch":"1415228400","pretty":"6:00 PM EST on November 05, 2014","civil":"6:00 PM","month_name":"November","month_name_abbrev":"Nov","weekday_name":"Wednesday","weekday_name_night":"Wednesday Night","weekday_name_abbrev":"Wed","weekday_name_unlang":"Wednesday","weekday_name_night_unlang":"Wednesday Night","ampm":"PM","tz":"","age":"","UTCDATE":""},"temp":{"english":"59","metric":"15"},"dewpoint":{"english":"37","metric":"3"},"condition":"Mostly Cloudy","icon":"mostlycloudy","icon_url":"http://icons.wxug.com/i/c/k/nt_mostlycloudy.gif","fctcode":"3","sky":"69","wspd":{"english":"5","metric":"8"},"wdir":{"dir":"WSW","degrees":"250"},"wx":"Mostly Cloudy","uvi":"0","humidity":"43","windchill":{"english":"-9999","metric":"-9999"},"heatindex":{"english":"-9999","metric":"-9999"},"feelslike":{"english":"59","metric":"15"},"qpf":{"english":"0.0","metric":"0"},"snow":{"english":"0.0","metric":"0"},"pop":"0","mslp":{"english":"30.12","metric":"1020"}},{"FCTTIME":{"hour":"19","hour_padded":"19","min":"00","min_unpadded":"0","sec":"0","year":"2014","mon":"11","mon_padded":"11","mon_abbrev":"Nov","mday":"5","mday_padded":"05","yday":"308","isdst":"0","epoch":"1415232000","pretty":"7:00 PM EST on November 05, 2014","civil":"7:00 PM","month_name":"November","month_name_abbrev":"Nov","weekday_name":"Wednesday","weekday_name_night":"Wednesday Night","weekday_name_abbrev":"Wed","weekday_name_unlang":"Wednesday","weekday_name_night_unlang":"Wednesday Night","ampm":"PM","tz":"","age":"","UTCDATE":""},"temp":{"english":"58","metric":"14"},"dewpoint":{"english":"37","metric":"3"},"condition":"Mostly Cloudy","icon":"mostlycloudy","icon_url":"http://icons.wxug.com/i/c/k/nt_mostlycloudy.gif","fctcode":"3","sky":"75","wspd":{"english":"4","metric":"6"},"wdir":{"dir":"WSW","degrees":"248"},"wx":"Mostly Cloudy","uvi":"0","humidity":"44","windchill":{"english":"-9999","metric":"-9999"},"heatindex":{"english":"-9999","metric":"-9999"},"feelslike":{"english":"58","metric":"14"},"qpf":{"english":"0.0","metric":"0"},"snow":{"english":"0.0","metric":"0"},"pop":"0","mslp":{"english":"30.14","metric":"1021"}},{"FCTTIME":{"hour":"20","hour_padded":"20","min":"00","min_unpadded":"0","sec":"0","year":"2014","mon":"11","mon_padded":"11","mon_abbrev":"Nov","mday":"5","mday_padded":"05","yday":"308","isdst":"0","epoch":"1415235600","pretty":"8:00 PM EST on November 05, 2014","civil":"8:00 PM","month_name":"November","month_name_abbrev":"Nov","weekday_name":"Wednesday","weekday_name_night":"Wednesday Night","weekday_name_abbrev":"Wed","weekday_name_unlang":"Wednesday","weekday_name_night_unlang":"Wednesday Night","ampm":"PM","tz":"","age":"","UTCDATE":""},"temp":{"english":"57","metric":"14"},"dewpoint":{"english":"38","metric":"3"},"condition":"Mostly Cloudy","icon":"mostlycloudy","icon_url":"http://icons.wxug.com/i/c/k/nt_mostlycloudy.gif","fctcode":"3","sky":"78","wspd":{"english":"4","metric":"6"},"wdir":{"dir":"WSW","degrees":"242"},"wx":"Mostly Cloudy","uvi":"0","humidity":"49","windchill":{"english":"-9999","metric":"-9999"},"heatindex":{"english":"-9999","metric":"-9999"},"feelslike":{"english":"57","metric":"14"},"qpf":{"english":"0.0","metric":"0"},"snow":{"english":"0.0","metric":"0"},"pop":"0","mslp":{"english":"30.15","metric":"1021"}},{"FCTTIME":{"hour":"21","hour_padded":"21","min":"00","min_unpadded":"0","sec":"0","year":"2014","mon":"11","mon_padded":"11","mon_abbrev":"Nov","mday":"5","mday_padded":"05","yday":"308","isdst":"0","epoch":"1415239200","pretty":"9:00 PM EST on November 05, 2014","civil":"9:00 PM","month_name":"November","month_name_abbrev":"Nov","weekday_name":"Wednesday","weekday_name_night":"Wednesday Night","weekday_name_abbrev":"Wed","weekday_name_unlang":"Wednesday","weekday_name_night_unlang":"Wednesday Night","ampm":"PM","tz":"","age":"","UTCDATE":""},"temp":{"english":"57","metric":"14"},"dewpoint":{"english":"39","metric":"4"},"condition":"Overcast","icon":"cloudy","icon_url":"http://icons.wxug.com/i/c/k/nt_cloudy.gif","fctcode":"4","sky":"83","wspd":{"english":"4","metric":"6"},"wdir":{"dir":"WSW","degrees":"241"},"wx":"Cloudy","uvi":"0","humidity":"50","windchill":{"english":"-9999","metric":"-9999"},"heatindex":{"english":"-9999","metric":"-9999"},"feelslike":{"english":"57","metric":"14"},"qpf":{"english":"0.0","metric":"0"},"snow":{"english":"0.0","metric":"0"},"pop":"0","mslp":{"english":"30.15","metric":"1021"}},{"FCTTIME":{"hour":"22","hour_padded":"22","min":"00","min_unpadded":"0","sec":"0","year":"2014","mon":"11","mon_padded":"11","mon_abbrev":"Nov","mday":"5","mday_padded":"05","yday":"308","isdst":"0","epoch":"1415242800","pretty":"10:00 PM EST on November 05, 2014","civil":"10:00 PM","month_name":"November","month_name_abbrev":"Nov","weekday_name":"Wednesday","weekday_name_night":"Wednesday Night","weekday_name_abbrev":"Wed","weekday_name_unlang":"Wednesday","weekday_name_night_unlang":"Wednesday Night","ampm":"PM","tz":"","age":"","UTCDATE":""},"temp":{"english":"56","metric":"13"},"dewpoint":{"english":"40","metric":"4"},"condition":"Overcast","icon":"cloudy","icon_url":"http://icons.wxug.com/i/c/k/nt_cloudy.gif","fctcode":"4","sky":"86","wspd":{"english":"3","metric":"5"},"wdir":{"dir":"WSW","degrees":"238"},"wx":"Cloudy","uvi":"0","humidity":"53","windchill":{"english":"-9999","metric":"-9999"},"heatindex":{"english":"-9999","metric":"-9999"},"feelslike":{"english":"56","metric":"13"},"qpf":{"english":"0.0","metric":"0"},"snow":{"english":"0.0","metric":"0"},"pop":"0","mslp":{"english":"30.15","metric":"1021"}}]}
+
+describe("WeatherChart", function(){
+  // The array we want to get all of our info from is located in `dummyData.hourly_forecast`
+  it("returns a list of farenheits", function(){
+    // Each Item has a property of temp. 
+    // The farenheit is stored in temp's english property
+    // Return a new array of just these values
+    expect(getFarenheits(dummyData)).toEqual(['59','58','57','57','56']);
+  });
+
+  it("returns a list of hours", function(){
+    // Each Item has a property of FCTTIME. 
+    // The hour is stored in FCTTIME's hour property
+    // Return a new array of just these values
+    expect(getHours(dummyData)).toEqual(['18','19','20','21','22']);
+  });
+
+  it("generates the right dataset for chart.js", function(){
+    expected = {
+      labels: ["label1", "label2"],
+      datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: ["data1", "data2"]
+      }
+      ]
+    };
+    expect(generateDataSet(["label1", "label2"], ["data1", "data2"])).toEqual(expected);
+  })
+
+  describe("Ajax Requests" , function(){
+    var request,  onSuccess;
+
+    it("Works", function(){
+      spyOn($, 'ajax');
+      onSuccess = jasmine.createSpy('onSuccess');
+
+      makeAjaxRequest("http://example.com", onSuccess)
+      expect($.ajax).toHaveBeenCalledWith({url: 'http://example.com', dataType: 'jsonp', success: onSuccess})
+    })
+  })
+})
