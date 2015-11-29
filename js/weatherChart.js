71a594c9bd790ee873ae9466a76734519dc477b9
Garett Arrowood <garettarrowood@gmail.com>
2015-11-29 15:31:04 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,52 +1,57 @@
-var API_KEY = "6f97229f7b7486ad";
-var URL = "http://api.wunderground.com/api/6f97229f7b7486ad/hourly/q/GA/Atlanta.json";
-var ctx = $('#ATLWeatherChart');
+// var API_KEY = "6f97229f7b7486ad";
+// var URL = "http://api.wunderground.com/api/6f97229f7b7486ad/hourly/q/GA/Atlanta.json";
+// var ctx = $('#ATLWeatherChart');
+jQuery(document).ready(function($) {
 
-function getFarenheits(json){
-  var farenheits = [];
-  for(var i=0; i<5;i++){
-    farenheits.push(json.hourly_forecast[i].temp.english);
+  function getFarenheits(json){
+    var farenheits = [];
+    for(var i=0; i<5;i++){
+      farenheits.push(json.hourly_forecast[i].temp.english);
+    }
+    return farenheits;
   }
-  return farenheits;
-}
 
-function getHours(json){
-  var hours = [];
-  for(var i=0; i<5;i++){
-    hours.push(json.hourly_forecast[i].FCTTIME.hour);
+  function getHours(json){
+    var hours = [];
+    for(var i=0; i<5;i++){
+      hours.push(json.hourly_forecast[i].FCTTIME.hour);
+    }
+    return hours;
   }
-  return hours;
-}
 
-function generateDataSet(hours, farenheits){
-  return {
-    labels: hours,
-    datasets: [
-      {
-        label: "Hourly Weather for Atlanta",
-        fillColor: "rgba(220,220,220,0.2)",
-        strokeColor: "rgba(220,220,220,1)",
-        pointColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
-        data: farenheits
-      }
-    ]
+  function generateDataSet(hours, farenheits){
+    return {
+      labels: hours,
+      datasets: [
+        {
+          label: "Hourly Weather for Atlanta",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: farenheits
+        }
+      ]
+    }
   }
-}
-var data = generateDataSet(getHours(json), getFarenheits(json));
 
-function makeAjaxRequest(url, ajaxCall){
-  $.ajax({
-    url: url,
-    dataType: 'jsonp',
-    success: ajaxCall
-  });
-}
+  function ajaxCall(json){
+    var data = generateDataSet(getHours(json), getFarenheits(json));
+    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true})
+  }
 
-function ajaxCall(json){
-  var data = generateDataSet(getHours(json), getFarenheits(json));
-  var tempChart = new Chart(ctx).Line(data, { bezierCurve: true})
-}
+  function makeAjaxRequest(url, ajaxCall){
+    console.log(json);
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: ajaxCall()
+    });
+  }
+  makeAjaxRequest(URL, ajaxCall);
+});
+
+
 
